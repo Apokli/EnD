aa094d70bc8bdb4d4f44a199a2fd978c64ef95df
Apokli <hybbenny@163.com>
2021-12-30 20:44:55 +0100

    task 1 and 2

--- main.m
+++ main.m
@@ -3,9 +3,8 @@ load("Data.mat");
 ens = A .* A * (s.' * s);   % energy of signals
 
 %% task 1 : known A and s
-lambda = 1;
-Pfa = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5];
-pred1 = zeros(length(Pfa), size(x, 1), size(A, 2));
+Pfa = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5];                   % 5 Pfa scenarios
+pred1 = zeros(length(Pfa), size(x, 1), size(A, 2));     % predictions under every case (5 * 10000 * 30)
 
 for i = 1 : length(Pfa)
     for j = 1 : size(A, 2)
@@ -18,8 +17,27 @@ end
 
 %% task 2 : plot Pd versus SNR
 SNRs = 10 * log10(ens / sigmaw2);
-pd = pred_rate(mask, pred1);
 labels = ["Pfa = 1e-1", "Pfa = 1e-2", "Pfa = 1e-3", "Pfa = 1e-4", "Pfa = 1e-5"];
+
+% actual prediction rate
+pd = zeros(size(pred1, 1), size(pred1, 3));
+for i = 1 : size(pd, 1)
+    for j = 1 : size(pd, 2)
+        pd(i, j) = sum(pred1(i, mask == 1, j)) / sum(mask);
+    end
+end
 plot_pd(pd, SNRs, labels, "actual Pd when known A and s");
+
+% theoretical prediction rate
+
+pd_theory = zeros(size(pd));
+for i = 1 : size(pd_theory, 1)
+    for j = 1 : size(pd_theory, 2)
+        tmp = -norminv(Pfa(i), 0, 1) - sqrt(ens(j) / sigmaw2);
+        tmp2 = normcdf(-norminv(Pfa(i), 0, 1) - sqrt(ens(j) / sigmaw2), 0, 1);
+        pd_theory(i, j) = 1 - normcdf(-norminv(Pfa(i), 0, 1) - sqrt(ens(j) / sigmaw2), 0, 1);
+    end
+end
+plot_pd(pd_theory, SNRs, labels, "theoretical Pd when known A and s")
 
 %% task 3 : unknown A and known s
--- pred_rate.m
+++
@@ -1,9 +0,0 @@
-function pr = pred_rate(real, pred)
-    pr = zeros(size(pred, 1), size(pred, 3));
-    for i = 1 : size(pred, 1)
-        for j = 1 : size(pred, 3)
-            pr(i, j) = sum(pred(i, real == 1, j)) / sum(real);
-        end
-    end
-end
-
